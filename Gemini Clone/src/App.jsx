b6b49c43687c29aff533b27ae0e19440664b1eae
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-08-04 17:50:59 -0700

    fixed logic to load frontend

--- Gemini Clone/src/App.jsx
+++ Gemini Clone/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 
 const App = () => {
+    console.log("Load app!!!")
   return (
     <>
     <Sidebar/>      
