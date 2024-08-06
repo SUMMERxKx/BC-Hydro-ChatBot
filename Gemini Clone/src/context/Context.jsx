27ff4265bd1760cedb840fc673eb459579d59972
Samar <130741241+SUMMERxKx@users.noreply.github.com>
2024-08-05 18:35:26 -0700

    Functional Requirement Completed

--- Gemini Clone/src/context/Context.jsx
+++ Gemini Clone/src/context/Context.jsx
@@ -17,13 +17,25 @@ const ContextProvider = (props) => {
             setResultData(prev=>prev+nextWord)
         }, 75*index)
     }
+    const newChat = () =>{
+        setLoading(false)
+        setShowResult(false)
+    }
     
     const onSent = async(prompt) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(true)
-        const response = await run(input)
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt)
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPrevPrompt(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
         
         let responseArr = response.split("**");
         let newResponse ="";
@@ -59,7 +71,8 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
     return (
         <Context.Provider value={contextValue}>
